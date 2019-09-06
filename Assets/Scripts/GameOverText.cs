b4d0fcd38ddc3f28b8783858207f430351672b3a
Ader Einstein <adereinstein1>
2019-09-06 11:06:09 -0700

    initial commit

---
+++ Assets/Scripts/GameOverText.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverText : MonoBehaviour {
+
+	public GameObject treasure;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+
+		// start text off as completely transparent black
+		text.color = new Color(0, 0, 0, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (treasure == null) {
+			// reveal text only when helicopter is null (destroyed)
+			text.color = new Color(0, 0, 0, 1);
+			text.text = "You Won! Press enter to play again.";
+			
+			// jump is space bar by default
+			if (Input.GetButtonDown("Jump")) {
+				// reload entire scene, starting music over again, refreshing score, etc.
+				SceneManager.LoadScene("ProBuilderScene");
+			}
+		}
+	}
+}
---
+++ Assets/treasure.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class treasure : MonoBehaviour
+{
+
+    public AudioSource pickup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+	void Update () {
+
+		transform.Rotate(0, 5f, 0, Space.World);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
